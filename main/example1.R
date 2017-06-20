494fa1056089cf580630ecd5225f162899508de5
Sangwon Hyun <shyun@cmu.edu>
2017-06-20 14:57:08 -0400

    Make opt=c(1,2,3) option for different ways to form the complement row space basis.

--- main/example1.R
+++ main/example1.R
@@ -2,12 +2,13 @@
 library(genlassoinf)
 ## set.seed(1)
 n = 12
-sigma = 4
+sigma = 1
 ngen = 300
 mn = rep(0,n)
+## mn = c(2,rep(0,n-1))
 
 
-onesim <- function(ii=NULL){
+onesim <- function(ii=NULL, opt=3){
 
     ## Generate data
     if(!is.null(ii))  set.seed(ii)
@@ -17,15 +18,16 @@ onesim <- function(ii=NULL){
     maxsteps = 2
     D = makeDmat(n,type='tf',ord=0)
     f0 = dualpathSvd2(y0, D=D, maxsteps, approx=T)
+    cp1 = f0$action[1]
+    cp2 = f0$action[2]
 
     ## Get naive 1-step poyhedron
     Gobj.naive = getGammat.naive(obj = f0, y = y0, condition.step = 1)
-    f0$nk
-
     G = Gobj.naive$G
     u = Gobj.naive$u
-    cps = f0$action[1]   ## cps = c(4,9)
+    cps = cp1##f0$action[1]   ## cps = c(4,9)
     inds = do.call(c,lapply(cps, function(cp)return(cp+c(-1,0,1))))
+    inds = inds[inds!=0]
     other.inds = (1:n)[-inds]
 
     ## Helper function
@@ -53,46 +55,59 @@ onesim <- function(ii=NULL){
     A = rbind(suff.rows, sat.rows)
 
     #### Build A_rest
-
-    ## Method 1: manual
-    rest.inds = unlist(sapply(plateaus, function(plt)plt[-1]))
-    A_rest= do.call(rbind, lapply(rest.inds, function(ind){v=rep(0,n); v[ind] = 1;return(v)}))
-
-    ## Method 2: svd
-    S = svd(t(A),nu=ncol(A))
-    nr = nrow(suff.rows) + nrow(sat.rows) + nrow(test.stat.row)
-    A_rest = t(S$u[,(nr+1):ncol(A)])
-
-    ## Method 3: smarter manual (null space row bases)
-    B = matrix(0, nrow = n-nrow(A), ncol = n)
-    Bblocks <- lapply(plateaus, function(plt){
-        b = matrix(0, nrow = length(plt)-1, ncol = n)
-        b[,plt] = makeDmat(m=length(plt), type="tf", ord=0)
-        return(b)
-    })
-    binds = c(0,cumsum(sapply(Bblocks, nrow)))
-    Bblocks.rownums = sapply(1:(length(binds)-1), function(ii){
-        (binds[ii]+1):binds[ii+1]
-    })
-    for(ii in 1:length(plateaus)){B[Bblocks.rownums[[ii]], ] = Bblocks[[ii]]}
+    if(opt==1){
+        ## Method 1: manual
+        rest.inds = unlist(sapply(plateaus, function(plt)plt[-1]))
+        A_rest= do.call(rbind, lapply(rest.inds, function(ind){v=rep(0,n); v[ind] = 1;return(v)}))
+    } else if (opt==2){
+        ## Method 2: svd (most generic!!!)
+        S = svd(t(A),nu=ncol(A))
+        nr = nrow(suff.rows) + nrow(sat.rows) ##+ nrow(test.stat.row)
+        A_rest = t(S$u[,(nr+1):ncol(A)])
+    } else if(opt==3){
+        ## Method 3: smarter manual (null space row bases)
+        B = matrix(0, nrow = n-nrow(A), ncol = n)
+        plateaus2 = plateaus[which(sapply(plateaus, length)!=1)]
+        Bblocks <- lapply(plateaus2, function(plt){
+            b = matrix(0, nrow = length(plt)-1, ncol = n)
+            if(nrow(b)!=0){
+                b[,plt] = makeDmat(m=length(plt), type="tf", ord=0)
+            }
+            return(b)
+        })
+        binds = c(0,cumsum(sapply(Bblocks, nrow)))
+        Bblocks.rownums = lapply(1:(length(binds)-1), function(ii){
+            as.numeric((binds[ii]+1):binds[ii+1])
+        })
+        for(ii in 1:length(plateaus2)){B[Bblocks.rownums[[ii]], ] = Bblocks[[ii]]}
+        A_rest = B
+    } else ( stop("not written!"))
 
     ## Build A_aug.
     Ag = rbind(A,A_rest)
 
-    ## Partition
-    Sigma = (Ag) %*% diag(rep(sigma^2,n)) %*% t(Ag)
-    Si = nrow(A)
-    S11 = Sigma[1:Si, 1:Si]
-    S12 = Sigma[1:Si, (Si+1):n]
-    S21 = Sigma[(Si+1):n, 1:Si]
-    S22 = Sigma[(Si+1):n, (Si+1):n]
-
     ## Make mean + covariance for W_rest | W
     w = A %*% y0
-    Sigmarest = S22-S21%*%solve(S11)%*%S12
-    murest = S21 %*% solve(S11) %*% cbind(w)
+
+    if(opt==3){
+        Sigmarest = B%*%t(B) * sigma^2
+        murest = rep(0,nrow(B))
+    } else {
+        ## Partition matrix into four blocks
+        Sigma = (Ag) %*% diag(rep(sigma^2,n)) %*% t(Ag)
+        Si = nrow(A)
+        S11 = Sigma[1:Si, 1:Si]
+        S12 = Sigma[1:Si, (Si+1):n]
+        S21 = Sigma[(Si+1):n, 1:Si]
+        S22 = Sigma[(Si+1):n, (Si+1):n]
+
+        ## Calculate using Schur's complement
+        Sigmarest = S22-S21%*%solve(S11)%*%S12
+        murest = S21 %*% solve(S11) %*% cbind(w)
+    }
 
     ## Draw ys
+    set.seed(0)
     wrests  = MASS::mvrnorm(n=ngen, mu=murest, Sigma=Sigmarest)
     ys = apply(wrests, 1, function(wrest){
         ysample = solve(Ag) %*% cbind(c(w,wrest))
@@ -100,57 +115,62 @@ onesim <- function(ii=NULL){
     })
 
     ## Check polyhedron
-    which.y.in.polyhedron =  apply(G%*%ys, 2, function(mycol){
-        all(as.numeric(mycol) > u)
-    })
+    check.poly=TRUE
+    if(check.poly){
+        which.y.in.polyhedron =  apply(G%*%ys, 2, function(mycol){
+            all(as.numeric(mycol) > u)
+        })
+        ys = ys[,which.y.in.polyhedron]
+    }
 
-    ys = ys[,which.y.in.polyhedron]
-
-    ## ## Take these samples, run the second step, collect the regularization parameter
-    ## maxsteps=2
-    ## D = makeDmat(n,type='tf',ord=0)
-    ## newlams = apply(ys, 2, function(mycol){
-    ##     ynew = as.numeric(mycol)
-    ##     fnew = dualpathSvd2(ynew, D=D, maxsteps, approx=T)
-    ##     newlambda = fnew$lambda[2]
-    ##     return(newlambda)
+    ## See that the next variable matches
+    ## cp2.matches = apply(ys, 2, function(mycol){
+    ##     fnew = dualpathSvd2(mycol, D=D, 2, approx=T)
+    ##     cp2new = fnew$action[2]
+    ##     return(cp2 == cp2new)
     ## })
+    ## ys = ys[,cp2.matches]
 
-    ## Take these samples, run the second step, calculate the linear contrast
-    maxsteps=2
+
+    ## Take these samples, run the second step, collect the regularization parameter
+    maxsteps = 2
     D = makeDmat(n,type='tf',ord=0)
-    newstat = apply(ys, 2, function(mycol){
-        ## fnew = dualpathSvd2(mycol, D=D, 2, approx=T)
-        ## cp1 = fnew$action[1]
-        ## cp2 = fnew$action[2]
-        ## ## d = sign(cp2)
-        ## d=1
-        ## if(cp2 > cp1){
-        ##     test.stat.row = make.contrast(cp1,cp2,n,n,d)
-        ## } else {
-        ##     test.stat.row = make.contrast(1,cp2,cp1,n,d)
-        ## }
-        ## print(rbind(test.stat.row) %*% cbind(mn))
-        test.stat.row = > E
-        return(rbind(test.stat.row)%*%cbind(mycol))
+    newlams = apply(ys, 2, function(mycol){
+        ynew = as.numeric(mycol)
+        fnew = dualpathSvd2(ynew, D=D, maxsteps, approx=T)
+        newlambda = fnew$lambda[2]
+        return(newlambda)
     })
-
 
     ## Get the current test statistic value
-    cp1 = f0$action[1]
-    cp2 = f0$action[2]
-    ## d = sign(cp2)
-    d=1
+    d = sign(cp2)
+    ## d = 1
     if(cp2 > cp1){
         test.stat.row = make.contrast(cp1,cp2,n,n,d)
     } else {
         test.stat.row = make.contrast(1,cp2,cp1,n,d)
     }
+
+    test.stat.row = rbind(c(1,rep(0,n-1))) ## Temporarily added, nonrandom statistic
     mystat = as.numeric(rbind(test.stat.row)%*%cbind(y0))
 
-    ## ## Visualize, get p-value
-    ## hist(newstat)
-    ## abline(v=mystat)
+    ## Take ys, run the second step, calculate the linear contrast
+    D = makeDmat(n,type='tf',ord=0)
+    newstat = apply(ys, 2, function(mycol){
+        fnew = dualpathSvd2(mycol, D=D, 2, approx=T)
+        cp1 = fnew$action[1]
+        cp2 = fnew$action[2]
+        d = sign(cp2)
+        ## d = 1
+        if(cp2 > cp1){
+            test.stat.row = make.contrast(cp1,cp2,n,n,d)
+        } else {
+            test.stat.row = make.contrast(1,cp2,cp1,n,d)
+        }
+        return(rbind(test.stat.row)%*%cbind(mycol))
+    })
+
+
 
     ## Calculate p-value
     pv = sum(newstat>mystat)/ncol(ys)
@@ -161,15 +181,22 @@ onesim <- function(ii=NULL){
 ## Actually run simulations
 nsim = 100
 pvs = rep(NA,nsim)
-for(ii in 100+(1:nsim)){
+## for(ii in 100+(1:nsim)){
+pvs = mclapply((1:nsim), function(isim){
     tryCatch({
-        print(ii)
-        pvs[ii] <- onesim(ii)
+        ## cat('\r', isim, 'out of', nsim)
+        return(onesim(isim,opt=2))
     }, error=function(e) {print("error occurred")})
-}
+}, mc.cores=3)
+
+## sapply(pvs, is.na)
+## which(sapply(pvs, function(pv)pv==1 | pv==0))
+pvs2 = unlist(pvs)
 
 
 ## Plot
-hist(pvs)
-qqplot(x=pvs,y=seq(from=0,to=1,length=nsim))
+hist(pvs2)
+qqplot(y=pvs2,x=seq(from=0,to=1,length=length(pvs2)),xlab = "expected", ylab="observed")
 abline(0,1)
+
+## Why do I have boundary cases?
